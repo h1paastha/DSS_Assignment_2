dd5f7d887fc4aec84734ab0bf394e7902daf21e3
root <root@AP>
2023-05-16 11:28:55 +0100

    Third Commit

--- passportConfig.js
+++ passportConfig.js
@@ -1,52 +1,52 @@
 const LocalStrategy = require("passport-local").Strategy;
+const passport = require('passport');
 const { pool } = require("./dbConfig");
 const bcrypt = require("bcrypt");
 const Joi = require('joi');
+const speakeasy = require('speakeasy');
+const qrcode = require('qrcode');
 ////////////////////////////////////////////////////////////////////////////////////////////
-function initialize(passport) {
-	console.log("Initialized");
-	const signinSchema = Joi.object({
-		email: Joi.string().email().required(),
-		password: Joi.string().min(12).max(30).required(),
-	});
-	const authenticateUser = (email, password, done) => {
-		console.log(email, password);
-		pool.query(`SELECT * FROM users WHERE email = $1`, [email], (err, results) => {
+passport.use(
+	new LocalStrategy({ 
+		usernameField: "email", 
+		passwordField: "password" , 
+	}, (email, password, done) => {
+		pool.query('SELECT * FROM users WHERE email = $1', [email], (err, result) => {
 			if (err) {
 				throw err;
 			}
-			console.log(results.rows);
-			if (results.rows.length > 0) {
-				const user = results.rows[0];
+			if (result.rows.length > 0) {
+				const user = result.rows[0];
 				bcrypt.compare(password, user.password, (err, isMatch) => {
-					if (err) {
+					if (err) { 
 						console.log(err);
 					}
 					if (isMatch) {
 						return done(null, user);
 					} else {
-						//password is incorrect
 						return done(null, false, { message: "Incorrect email address or password" });
 					}
 				});
 			} else {
-			// No user
-				return done(null, false, {message: "Incorrect email address or password"});
+				return done(null, false, { 
+					message: "Incorrect email address or password" 
+				}); 
 			}
-		});
-	};
-	passport.use(
-		new LocalStrategy({ usernameField: "email", passwordField: "password" }, authenticateUser)
-	);
-	passport.serializeUser((user, done) => done(null, user.id));
-	passport.deserializeUser((id, done) => {
-		pool.query(`SELECT * FROM users WHERE id = $1`, [id], (err, results) => {
-			if (err) {
-				return done(err);
-			}
-			console.log(`ID is ${results.rows[0].id}`);
-			return done(null, results.rows[0]);
-		});
+			});
+}));
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+	pool.query(`SELECT * FROM users WHERE id = $1`, [id], (err, results) => {
+		if (err) {
+			return done(err);
+		}
+		console.log(`ID is ${results.rows[0].id}`);
+		return done(null, results.rows[0]);
 	});
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function initialize(passport) {
+	console.log("Initialized");
 }
 module.exports = initialize;
--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const sgMail = require('@sendgrid/mail');
 const { pool } = require("./dbConfig");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
-const randomstring = require('randomstring');
 const bodyParser = require("body-parser");
-const speakeasy = require('speakeasy');
-const QRCode = require('qrcode');
 const cookieParser = require('cookieparser');
+const crypto = require('crypto');
 const pg = require('pg');
+const passwordValidator = require('password-validator');
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 const initializePassport = require("./passportConfig");
 initializePassport(passport);
+const API_KEY = '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';
+sgMail.setApiKey(API_KEY);
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
@@ -49,6 +52,15 @@ app.get('/users/logout', function(req, res, next) {
 	});
 });
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+const passwordSchema = new passwordValidator();
+passwordSchema
+.is().min(12)
+.is().max(50)
+.has().uppercase()
+.has().lowercase()
+.has().digits()
+.has().symbols();
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.post("/users/register", async (req, res) => {
 	let { name, email, password, password2 } = req.body;
 	let errors = [];
@@ -61,6 +73,9 @@ app.post("/users/register", async (req, res) => {
 	}
 	if (password !== password2) {
 		errors.push({ message: "Passwords do not match" });
+	}
+	if (!passwordSchema.validate(password)) {
+		errors.push({ message: "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit and a special character" });
 	}
 	if (errors.length > 0) {
 		res.render("register", { errors, name, email, password, password2 });
@@ -91,7 +106,7 @@ app.post("/users/register", async (req, res) => {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.post("/users/login", 
 	passport.authenticate("local", { 
-		successRedirect: "/users/dashboard", 
+		successRedirect: "/users/2fa", 
 		failureRedirect: "/users/login", 
 		failureFlash: true 
 	})
@@ -100,10 +115,6 @@ app.post("/users/login",
 app.post("/users/dashboard", (req, res) => {
 	const { title, description } = req.body;
 	const personName = req.user.name;
-	const token = req.csrfToken();
-	if (req.body._csrf !== token) {
-		return res.status(403).send('CSRF token invalid');
-	}
 	pool.query('INSERT INTO posts (title, description, name) VALUES ($1, $2, $3)',
 		[title, description, personName], (err, result) => {
 	if (err) { 
@@ -135,6 +146,46 @@ app.get('/search', async (req, res) => {
 	const result = await pool.query('SELECT * FROM posts WHERE name ILIKE $1', [`%${query}%`]);
 	console.log(result.rows);
 	res.json(result.rows);
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/users/2fa', checkNotAuthenticated, async (req, res) => {
+	const email = req.user.email;
+	const userId = req.user.id;
+	const token = crypto.randomBytes(64).toString('hex');
+	await pool.query(
+		'UPDATE users SET verification_token = $1, verified = $2 WHERE id = $3', [token, false, userId]
+	);
+	const verificationLink = `http://${req.headers.host}/users/verify?token=${token}`;
+	const msg = {
+		from: {
+			name: 'UEA Blog',
+			email: 'a.pareek@uea.ac.uk'
+		},
+		to: email,
+		subject: 'Verification Link',
+		html: `Click the following link to verify your email: <a href="${verificationLink}">${verificationLink}</a>`
+	};
+	await sgMail.send(msg);
+	res.render('login.ejs', { successmsg: "Check your email for login URL" });
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/users/verify', async (req, res) => {
+	const { token } = req.query;
+	try {
+		const query = 'SELECT * FROM users WHERE verification_token = $1';
+		const result = await pool.query(query, [token]);
+		if (result.rowCount === 0) {
+			res.status(400).json({ message: 'Invalid token. Please try again.' });
+		} else {
+			const updateQuery = 'UPDATE users SET verified = true WHERE verification_token = $1';
+			await pool.query(updateQuery, [token]);
+			res.render("dashboard");
+//			res.status(200).json({ message: 'Verification successful. Please log in.' });
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: 'Verification failed. Please try again later.' });
+	}
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function checkAuthenticated(req, res, next) {
